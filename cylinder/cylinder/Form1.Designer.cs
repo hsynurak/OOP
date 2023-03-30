8feecd6c1e5c0c99ecbce996696521d9f46c8446
huseyinurak <102410337+hsynurak@users.noreply.github.com>
2023-03-30 21:25:26 +0300

    Add files via upload

---
+++ cylinder/cylinder/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace cylinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.txtSurfArea = new System.Windows.Forms.TextBox();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblSurfArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "calc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(29, 131);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 1;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(176, 131);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(437, 134);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(100, 20);
+            this.txtVolume.TabIndex = 3;
+            // 
+            // txtSurfArea
+            // 
+            this.txtSurfArea.Location = new System.Drawing.Point(598, 134);
+            this.txtSurfArea.Name = "txtSurfArea";
+            this.txtSurfArea.Size = new System.Drawing.Size(100, 20);
+            this.txtSurfArea.TabIndex = 4;
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(59, 115);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(40, 13);
+            this.lblRadius.TabIndex = 5;
+            this.lblRadius.Text = "Radius";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(207, 115);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 6;
+            this.lblHeight.Text = "Height";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(463, 118);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(42, 13);
+            this.lblVolume.TabIndex = 7;
+            this.lblVolume.Text = "Volume";
+            // 
+            // lblSurfArea
+            // 
+            this.lblSurfArea.AutoSize = true;
+            this.lblSurfArea.Location = new System.Drawing.Point(619, 118);
+            this.lblSurfArea.Name = "lblSurfArea";
+            this.lblSurfArea.Size = new System.Drawing.Size(51, 13);
+            this.lblSurfArea.TabIndex = 8;
+            this.lblSurfArea.Text = "Surf Area";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSurfArea);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.txtSurfArea);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.TextBox txtSurfArea;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblSurfArea;
+    }
+}
+
---
+++ cylinder/cylinder/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cylinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            my_cylinder my_Cylinder = new my_cylinder();
+            
+            my_Cylinder.R = 1;
+            my_Cylinder.H = 1;
+            double.TryParse(txtRadius.Text, out my_Cylinder.R);
+            double.TryParse(txtHeight.Text, out my_Cylinder.H);
+
+            double V;
+            double SA;
+
+            V = my_Cylinder.volume();
+            SA = my_Cylinder.surface_area();
+
+            txtVolume.Text = V.ToString();
+            txtSurfArea.Text = SA.ToString();
+
+        }
+    }
+
+    public class my_cylinder
+    {
+        public double R;
+        public double H;
+
+        public double volume()
+        {
+            double ret = 3.14 * R * R * H;
+            return ret;
+        }
+
+        public double surface_area()
+        {
+            double ret = (2 * 3.14 * R * R) + (2 * 3.14 * R * R * H);
+            return ret;
+        }
+    }
+}
